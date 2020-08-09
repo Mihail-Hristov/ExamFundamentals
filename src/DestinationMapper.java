5635eb4819797bf200cd950d5993929b0b167c3d
Mihail Hristov <mis6us@icloud.com>
2020-08-09 09:57:24 +0300

    Add src code

--- src/DestinationMapper.java
+++ src/DestinationMapper.java
@@ -1,10 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DestinationMapper {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String regex = "([=\\/]{1})(?<place>[A-Z][A-Za-z]{2,})\\1";
+        Pattern pattern = Pattern.compile(regex);
+        List<String> locations = new ArrayList<>();
 
+        int travelPoints = 0;
+
+        String input = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            String currentLocation = matcher.group("place");
+            locations.add(currentLocation);
+            int currentPoints = currentLocation.length();
+
+            travelPoints += currentPoints;
+        }
+
+        System.out.print("Destinations: ");
+        System.out.println(locations.toString().replaceAll("[\\[\\]]",""));
+
+        System.out.println(String.format("Travel Points: %d", travelPoints));
 
     }
 }
